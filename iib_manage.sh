cf088aeb8a9cea88042872b1d4c93d556a1651d9
Pablo Jesus Pedraza <37515166+wayaba@users.noreply.github.com>
2018-12-03 10:55:41 -0300

    Update iib_manage.sh

--- iib_manage.sh
+++ iib_manage.sh
@@ -33,6 +33,16 @@ start_iib()
 
 
 	if [ ${NODE_EXISTS} -ne 0 ]; then
+	  echo "Execute pre custom config (/tmp/script/preconfig.sh)"
+	  echo "----------------------------------------"
+          echo "----------------------------------------"
+		  if [ -f /tmp/script/preconfig.sh ]
+		  then
+			echo "ejecutando pre........."
+			/tmp/script/preconfig.sh
+			echo "fin sh pre........."
+		  fi
+	  echo "----------------------------------------"
           echo "----------------------------------------"
           echo "Node $NODENAME does not exist..."
           echo "Creating node $NODENAME"
@@ -57,16 +67,16 @@ start_iib()
           done		  
           echo "----------------------------------------"
           echo "----------------------------------------"
-		  echo "Execute post custom config (/tmp/script/postconfig.sh)"
-		  echo "----------------------------------------"
+	  echo "Execute post custom config (/tmp/script/postconfig.sh)"
+	  echo "----------------------------------------"
           echo "----------------------------------------"
 		  if [ -f /tmp/script/postconfig.sh ]
 		  then
-			echo "ejecutando........."
+			echo "ejecutando post........."
 			/tmp/script/postconfig.sh
 			echo "fin sh post........."
 		  fi
-		  echo "----------------------------------------"
+	  echo "----------------------------------------"
           echo "----------------------------------------"
 	else
           echo "----------------------------------------"
